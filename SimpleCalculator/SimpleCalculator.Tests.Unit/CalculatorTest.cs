55f7bff9fcdae0856b25ee35bdeb8d3de17b5fab
Derek Donoghue <45354824+Derekdonoghue@users.noreply.github.com>
2019-02-20 18:10:37 +0000

    CA3

---
+++ SimpleCalculator/SimpleCalculator.Tests.Unit/CalculatorTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Tests.Unit
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void AddTwoNumbers()
+        {
+            double result = Calculator.Calculate(1, 2, "+");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void SubtractTwoNumbers()
+        {
+            double result = Calculator.Calculate(3, 2, "-");
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void MultiplyTwoNumbers()
+        {
+            double result = Calculator.Calculate(1, 2, "*");
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void DivideTwoNumbers()
+        {
+            double result = Calculator.Calculate(4, 2, "/");
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Squarednumber()
+        {
+            double result = Calculator.Calculate(10, 10, "s");
+            Assert.AreEqual(100, result);
+
+        }
+
+
+        [TestMethod]
+        public void Squareroot()
+        {
+            double result = Calculator.Calculate(100, 10, "r");
+            Assert.AreEqual(10, result);
+
+        }
+
+        [TestMethod]
+        public void CosNumber()
+        {
+            double result = Calculator.Calculate(10, 0, "c");
+            Assert.AreEqual(-1, result);
+
+        }
+
+        [TestMethod]
+        public void TanNumber()
+        {
+            double result = Calculator.Calculate(10, 0, "t");
+            Assert.AreEqual(1, result);
+
+        }
+
+        [TestMethod]
+        public void SinNumber()
+        {
+            double result = Calculator.Calculate(10, 0, "i");
+            Assert.AreEqual(-1, result);
+
+        }
+
+        [TestMethod]
+        public void RemainderNumber()
+        {
+            double result = Calculator.Calculate(10, 10, "%");
+            Assert.AreEqual(0, result);
+
+        }
+
+
+
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void ConfirmInvalidSymbolicOperationsAreNotSupported_Completely()
+        {
+            double result = Calculator.Calculate(5, 10, "&");
+        }
+
+
+
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class Calculator
+    {
+        public static double Calculate(double firstNum, double secondNum, string op)
+        {
+            double result;
+            switch (op)
+            {
+                case "+":
+                    result = firstNum + secondNum;
+                    break;
+                case "-":
+                    result = firstNum - secondNum;
+                    break;
+                case "*":
+                    result = firstNum * secondNum;
+                    break;
+                case "/":
+                    result = firstNum / secondNum;
+                    break;
+                case "s":
+                    result = (int)Math.Pow(firstNum, 2);
+                    break;
+                case "r":
+                    result = Math.Sqrt(firstNum);
+                    break;
+                case "%":
+                    result = firstNum % secondNum;
+                    break;
+                case "c":
+                    result = Math.Round(Math.Cos(firstNum));
+                    break;
+                case "t":
+                    result = Math.Round(Math.Tan(firstNum));
+                    break;
+                case "i":
+                    result = Math.Round(Math.Sin(firstNum));
+                    break;
+
+                default:
+                    throw new NotSupportedException("Specified operation is not supported");
+            }
+            return result;
+        }
+    }
+}
